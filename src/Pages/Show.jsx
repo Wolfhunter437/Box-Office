d61f187d1e1837b5eafa786d528b8c2d1749f240
Wolfhunter <utkarshjainx1@gmail.com>
2024-01-03 16:22:00 +0530

    extract data fetching login into a custom hook useShowById

--- src/Pages/Show.jsx
+++ src/Pages/Show.jsx
@@ -2,24 +2,29 @@ import { useEffect, useState } from 'react';
 import { useParams} from 'react-router-dom';
 import { getShowById } from '../Api/tvmaze';
 
-export default function Show() {
-
-    const {showId} = useParams();
-
-    const [showData, setShowData] = useState(null);
-    const [showError, setShowError] = useState(null);
-
-    useEffect(() => {
-      async function fetchData() {
-        try {
-          const data = await getShowById(showId);
-          setShowData(data);
-        } catch (err) {
-          setShowError(err);
-        }
+const useShowById = (showId) => {
+  
+  const [showData, setShowData] = useState(null);
+  const [showError, setShowError] = useState(null);
+  
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await getShowById(showId);
+        setShowData(data);
+      } catch (err) {
+        setShowError(err);
       }
-      fetchData()
-    },[showId])
+    }
+    fetchData()
+  },[showId])
+  
+  return { showData, showError };
+}
+
+export default function Show() {
+  const {showId} = useParams();
+  const {showData, showError} = useShowById(showId);
 
     if(showError){
       return <div>We have an error: {showError.message}</div>
